1c6a04c86db3bb3753aa140d59f1ef4954847c29
Arbaz Khan <ak2208803@gmail.com>
2025-05-24 11:53:52 +0530

    Before running rename-unsafe-lifecycles codemod

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Visualizer from "./Visualizer";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import { BrowserRouter } from "react-router-dom";
+
+const basename =
+  process.env.NODE_ENV === "production" ? "/JasonFengGit.github.io" : "/";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter basename={basename}>
+        <Visualizer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
