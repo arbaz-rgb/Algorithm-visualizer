fbc933ef8f2f6e53e739c5043458dc7845de0889
Arbaz Khan <ak2208803@gmail.com>
2025-05-25 11:39:14 +0530

    updated the workin of the size and speed dropdown

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "./SortingVisualizer.css";
 import Pile from "./Pile/Pile";
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import {
   selectionSort,
   bubbleSort,
@@ -51,9 +54,13 @@ export default class SortingVisualizer extends Component {
         quickSort,
       ],
     };
+
     this.randomizePiles = this.randomizePiles.bind(this);
     this.visualizeSorting = this.visualizeSorting.bind(this);
     this.setAlgorithm = this.setAlgorithm.bind(this);
+    this.setSpeed = this.setSpeed.bind(this);
+    this.setSize = this.setSize.bind(this);
+
     this.props.getFunctions(
       this.visualizeSorting,
       this.randomizePiles,
@@ -63,7 +70,7 @@ export default class SortingVisualizer extends Component {
   }
 
   componentDidMount() {
-    const piles = this.initializePiles();
+    const piles = this.initializePiles(this.state.numPiles);
     this.setState({
       piles: piles,
       unsortedPiles: piles.slice(),
@@ -82,19 +89,20 @@ export default class SortingVisualizer extends Component {
     this.setState({ currentAlgorithm: algoId });
   }
 
-  initializePiles() {
+  initializePiles(numPiles = this.state.numPiles) {
     let piles = [];
-    for (let i = 0; i < this.state.numPiles; i++) {
+    for (let i = 0; i < numPiles; i++) {
       piles.push(i + 5);
     }
 
-    for (let i = 0; i < this.state.numPiles; i++) {
+    for (let i = 0; i < numPiles; i++) {
       let j = getRandomInt(0, i);
       let temp = piles[i];
       piles[i] = piles[j];
       piles[j] = temp;
     }
-    piles.push(this.state.numPiles + 5);
+
+    piles.push(numPiles + 5); // dummy pile
     return piles;
   }
 
@@ -116,6 +124,8 @@ export default class SortingVisualizer extends Component {
     const statesInOrder =
       this.state.sortingAlgorithms[this.state.currentAlgorithm](piles);
 
+    const delay = this.state.pileDelayTimes[this.state.currentAlgorithm];
+
     for (let i = 0; i < statesInOrder.length; i++) {
       const { piles: state, changing: changingPiles, pivot } = statesInOrder[i];
       setTimeout(() => {
@@ -124,18 +134,18 @@ export default class SortingVisualizer extends Component {
           changingPiles: changingPiles,
           pivot: pivot,
         });
-      }, this.state.pileDelayTimes[this.state.currentAlgorithm] * i);
+      }, delay * i);
     }
 
     setTimeout(() => {
       this.setState({ rendering: false, finished: true });
       this.props.setVisualizerRendering(false);
-    }, this.state.pileDelayTimes[this.state.currentAlgorithm] * statesInOrder.length);
+    }, delay * statesInOrder.length);
   }
 
   randomizePiles() {
     if (this.state.rendering) return;
-    const piles = this.initializePiles();
+    const piles = this.initializePiles(this.state.numPiles);
     this.setState({
       finished: false,
       changingPiles: [],
@@ -155,10 +165,11 @@ export default class SortingVisualizer extends Component {
   setSize(s) {
     if (this.state.size === s) return;
     let sizes = { small: 20, median: 30, large: 40 };
-    const piles = this.initializePiles();
+    const numPiles = sizes[s];
+    const piles = this.initializePiles(numPiles);
     this.setState({
       size: s,
-      numPiles: sizes[s],
+      numPiles: numPiles,
       finished: false,
       changingPiles: [],
       piles: piles,
@@ -190,59 +201,49 @@ export default class SortingVisualizer extends Component {
           ))}
         </div>
 
+        {/* dropdown start */}
+
         <div className="d-flex control-panel">
-          <div className="dropdown">
-            <button
-              className="btn btn-outline-dark dropdown-toggle"
-              type="button"
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="outline-dark"
               disabled={this.state.rendering}
-              id="dropdownMenuSpeed"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
+              id="dropdown-speed"
+              className="navbtn"
             >
               {`Speed: ${this.state.speed}`}
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuSpeed">
-              {["slow", "median", "fast"].map((speed) => (
-                <button
-                  key={speed}
-                  type="button"
-                  className="btn btn-light navbtn"
-                  onClick={() => this.setSpeed(speed)}
-                >
-                  {speed}
-                </button>
-              ))}
-            </div>
-          </div>
+            </Dropdown.Toggle>
 
-          <div className="dropdown">
-            <button
-              className="btn btn-outline-dark dropdown-toggle"
-              type="button"
+            <Dropdown.Menu>
+              {["slow", "median", "fast"].map((speed) => (
+                <Dropdown.Item key={speed} onClick={() => this.setSpeed(speed)}>
+                  {speed}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="outline-dark"
               disabled={this.state.rendering}
-              id="dropdownMenuSize"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
+              id="dropdown-size"
+              className="navbtn"
             >
               {`Size: ${this.state.size}`}
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuSize">
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
               {["small", "median", "large"].map((size) => (
-                <button
-                  key={size}
-                  type="button"
-                  className="btn btn-light navbtn"
-                  onClick={() => this.setSize(size)}
-                >
+                <Dropdown.Item key={size} onClick={() => this.setSize(size)}>
                   {size}
-                </button>
+                </Dropdown.Item>
               ))}
-            </div>
-          </div>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
+
+        {/* dropdown end */}
 
         <h6 className="algoDescription">
           {this.state.currentAlgorithm === -1
