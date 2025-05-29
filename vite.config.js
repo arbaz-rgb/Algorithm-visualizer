0e7ff3f29710f315cd32f19a3bf5c964e3f9bb2d
itzgaurav2005 <133147299+itzgaurav2005@users.noreply.github.com>
2025-05-29 16:58:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/algorithm-visualizer/" : "/",
+  base: '/', // or '/your-repo-name/' if deploying to a subpath
 });
